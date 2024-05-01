9ed5c2108f29ac25eb9aa504306a1f6bf7c489bf
fsorodrigues <fsorodrigues@gmail.com>
2024-05-01 21:09:07 +1200

    Transfer logic to tripping package

--- parser/main.go
+++ parser/main.go
@@ -1,37 +1,16 @@
-package main 
+package main
 
 import (
-  "fmt"
-  "time"
-  "os"
-  utils "you-gotta-go/parser/utils"
-  trip "you-gotta-go/parser/tripping"
+	"fmt"
+	trip "you-gotta-go/parser/tripping"
+	utils "you-gotta-go/parser/utils"
 )
 
 func main() {
   var data utils.InputData = utils.Read()
   var trips []utils.Trip = utils.FilterByService(data.Trips, "23")
-  var message string
 
   if data.Closed || len(data.Trips) < 1 {
-    message = "No trips available"
-    fmt.Println(message)
-    os.Exit(0)
   }
 
-  var NextTrip utils.Trip = trip.GetNextTrip(trips)
-  now := time.Now()
-
-  if !NextTrip.Monitored {
-    message = "Bus not tracking. Gotta use the aimed time"
-    fmt.Println(message)
-    os.Exit(0)
-  }
-  
-  message = "%s: %s"
-  fmt.Println(fmt.Sprintf(
-    message, 
-    NextTrip.ServiceID,
-    NextTrip.Arrival.Expected.Time.Sub(now),
-  ))
 }
--- parser/tripping/tripping.go
+++ parser/tripping/tripping.go
@@ -1,7 +1,10 @@
 package tripping
 
 import (
+	"fmt"
 	"sort"
+	"time"
+  "strconv"
 	utils "you-gotta-go/parser/utils"
 )
 
@@ -16,4 +19,46 @@ func GetNextTrip(data []utils.Trip) utils.Trip {
   })
   
   return d[0]
+}
+
+func formatTime(d time.Duration) string {
+  return strconv.FormatFloat(d.Seconds() / 60, 'f', 0, 64)
+}
+
+func decideScenario(trip utils.Trip) string {
+  if !trip.Monitored { return "not-tracking" }
+  if trip.Status == "cancelled" { return "cancelled" }
+  if trip.Status == "delayed" { return "delayed" }
+  if trip.Status == "early" { return "early" }
+  return "on-time"
+}
+
+func CreateMessageFromTrip(scn string, trip utils.Trip) string {
+  var diff time.Duration
+  now := time.Now()
+
+  switch scn {
+  case "not-tracking":
+    diff = trip.Arrival.Aimed.Time.Sub(now)
+    return fmt.Sprintf("%s: %sm nt", trip.ServiceID, formatTime(diff))
+  case "cancelled":
+   return fmt.Sprintf("%s: CANC", trip.ServiceID)
+  case "delayed":
+    diff = trip.Arrival.Expected.Time.Sub(now)
+    return fmt.Sprintf("%s: %s del", trip.ServiceID, formatTime(diff))
+  case "early":
+    diff = trip.Arrival.Expected.Time.Sub(now)
+    return fmt.Sprintf("%s: %sm ear", trip.ServiceID, formatTime(diff))
+  case "on-time":
+    diff = trip.Arrival.Expected.Time.Sub(now)
+    return fmt.Sprintf("%s: %sm", trip.ServiceID, formatTime(diff))
+  default:
+    return "ERROR. Scenario not recognized"
+  }
+ 
+}
+
+func ParseTrip(trip utils.Trip, msg *string) {
+  scenario := decideScenario(trip)
+  *msg = CreateMessageFromTrip(scenario, trip) 
 }
