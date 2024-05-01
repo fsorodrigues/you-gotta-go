a814aa281faa86c57b612e3a1ea0f4c9b0f7dbd1
fsorodrigues <fsorodrigues@gmail.com>
2024-05-01 21:09:21 +1200

    Parse trip into message

--- parser/main.go
+++ parser/main.go
@@ -9,8 +9,14 @@ import (
 func main() {
   var data utils.InputData = utils.Read()
   var trips []utils.Trip = utils.FilterByService(data.Trips, "23")
+  var message *string = new(string)
 
   if data.Closed || len(data.Trips) < 1 {
+    *message = "No trips available"
+  } else {
+    var NextTrip utils.Trip = trip.GetNextTrip(trips)
+    trip.ParseTrip(NextTrip, message)
   }
 
+  fmt.Println(*message)
 }
