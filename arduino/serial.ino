a0dcaa9cb7cc4e3fb498371f8c55ffcc59877488
fsorodrigues <fsorodrigues@gmail.com>
2024-05-05 19:33:55 +1200

    Add Arduino code
    This is a WIP

---
+++ arduino/serial.ino
@@ -0,0 +1,85 @@
+#include <string.h>
+
+const byte numBytes = 32;
+byte receivedBytes[numBytes];
+byte displayBytes[numBytes];
+byte numReceived = 0;
+boolean newData = false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  recvBytesWithStartEndMarkers();
+  getNewData();
+  printData();
+}
+
+void recvBytesWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  byte startMarker = 0x3C;
+  byte endMarker = 0x3E;
+  byte rb;
+  
+
+  while (Serial.available() > 0 && newData == false) {
+    rb = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rb != endMarker) {
+        receivedBytes[ndx] = rb;
+        ndx++;
+        if (ndx >= numBytes) {
+            ndx = numBytes - 1;
+        }
+      }
+      else {
+        receivedBytes[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        numReceived = ndx;  // save the number for use when printing
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rb == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+void getNewData() {
+  if (newData == true) {
+    Serial.print("Incoming new data... ");
+    Serial.print(numReceived);
+    Serial.print(" bytes received");
+    Serial.println();
+  
+    for (byte ndx = 0; ndx < numBytes; ndx++) {
+      displayBytes[ndx] = receivedBytes[ndx];
+    }
+
+    newData = false;
+  }
+}
+
+void printData() {
+  char* msg = (char *)displayBytes;
+
+  if (strncmp("23:", msg, 3) == 0) {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(500);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(500);    
+  }
+}
+
+// Serial.print((char *)displayBytes);
+// Serial.print(" ");
+// Serial.print(strcmp((char *)displayBytes, "10"));
+// Serial.println();
