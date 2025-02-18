00139797a47b2ec041f965c15d38a1527f097189
fsorodrigues <fsorodrigues@gmail.com>
2025-02-18 21:11:10 +1300

    feat: listen to messages from a connection
    Enable reading incoming messages from a connection that implements the io.ReadWriteCloser interface.
    
    Currently, the newly re-architected application only accepts TCP (net package) connections. The plan is that in the near future it will be able to also interface with a Serial connection.

--- cmd/broadcaster/comms.go
+++ cmd/broadcaster/comms.go
@@ -2,14 +2,96 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"you-gotta-go/cmd/broadcaster/messages"
 )
 
 type Comms struct {
-	API_KEY  string
-	BASE_URL string
-	TCP_PORT string
+	API_KEY        string
+	BASE_URL       string
+	TCP_PORT       string
+	HoldingBuffer  []byte
+	IncomingMsg    messages.MsgBuf
+	BytesRead      int
+	BytesAvailable int
+	Reading        bool
+}
+
+func (c *Comms) clearIncomingMsg() {
+	c.IncomingMsg.Msg.Reset()
+}
+
+func (c *Comms) walkBuffer(conn io.ReadWriteCloser) {
+	b := make([]byte, c.BytesAvailable)
+	n := 0
+	startByte := 0
+	stopByte := 0
+
+	for i := c.BytesRead; i < c.BytesRead+c.BytesAvailable; i++ {
+		stopByte++
+		c.BytesRead++
+		c.BytesAvailable--
+
+		if c.HoldingBuffer[i] == 60 {
+			c.clearIncomingMsg()
+			c.IncomingMsg.MsgComplete = false
+			b[n] = c.HoldingBuffer[i]
+			startByte = n
+			stopByte = n + 1
+			n++
+			continue
+		}
+
+		if c.HoldingBuffer[i] == 62 {
+			b[n] = c.HoldingBuffer[i]
+			c.Reading = false
+			c.IncomingMsg.MsgComplete = true
+			break
+		}
+
+		if !c.IncomingMsg.MsgComplete {
+			b[n] = c.HoldingBuffer[i]
+			n++
+		}
+	}
+
+	c.IncomingMsg.Msg.Write(b[startByte:stopByte])
+	if c.Reading {
+		c.readToBuffer(conn)
+	}
+}
+
+func (c *Comms) readToBuffer(conn io.ReadWriteCloser) {
+	n, err := conn.Read(c.HoldingBuffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.BytesRead = 0
+	c.BytesAvailable = n
+}
+
+func (c *Comms) ReadFromConnection(conn io.ReadWriteCloser) {
+	c.Reading = true
+
+	for c.Reading {
+		c.walkBuffer(conn)
+		if c.BytesAvailable == 0 {
+			c.Reading = false
+			break
+		}
+	}
+}
+
+func (c *Comms) handleConnection(conn io.ReadWriteCloser) {
+	// try reading from connection
+	c.ReadFromConnection(conn)
+
+	// after reading kick off routine specified in incoming message
+	fmt.Println(string(c.IncomingMsg.Msg.Bytes()[:]))
+
+	conn.Close()
 }
 
 func (c *Comms) Listen() {
@@ -25,16 +107,6 @@ func (c *Comms) Listen() {
 			// handle error
 			log.Fatalln("Error accepting incoming TCP connection", err)
 		}
-		go handleConnection(conn)
+		go c.handleConnection(conn)
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	msg := "hello"
-
-	encodedMsg := []byte(msg)
-
-	conn.Write(encodedMsg)
-
-	conn.Close()
 }
--- cmd/broadcaster/main.go
+++ cmd/broadcaster/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"you-gotta-go/cmd/broadcaster/messages"
 
 	// "you-gotta-go/cmd/scraper"
 
@@ -31,11 +33,17 @@ func init() {
 func main() {
 	API_KEY := os.Getenv("API_KEY")
 	BASE_URL := os.Getenv("BASE_URL")
+	TCP_PORT := os.Getenv("TCP_PORT")
 
 	app := Comms{
-		API_KEY:  API_KEY,
-		BASE_URL: BASE_URL,
-		TCP_PORT: "1108",
+		API_KEY:        API_KEY,
+		BASE_URL:       BASE_URL,
+		TCP_PORT:       TCP_PORT,
+		HoldingBuffer:  make([]byte, 25),
+		IncomingMsg:    messages.MsgBuf{Msg: bytes.Buffer{}, MsgComplete: false},
+		BytesAvailable: 0,
+		BytesRead:      0,
+		Reading:        false,
 	}
 
 	app.Listen()
